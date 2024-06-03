c6333e57c01224b6662dd4cde6d528b4ca543164
景军 <rxswift@126.com>
2024-06-03 23:34:46 +0800

    发送验证码功能

--- pet-project/handler/api_handler/user_handler.go
+++ pet-project/handler/api_handler/user_handler.go
@@ -2,29 +2,48 @@ package handler
 
 import (
 	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/gomail.v2"
+	"gorm.io/gorm"
+	"math/rand"
 	"net/http"
 	"pet-project/db"
 	"pet-project/middleware"
 	"pet-project/models"
 	"pet-project/response"
 	"pet-project/util"
-	"strconv"
-
-	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
 func GetTencentCode(c *gin.Context) {
 	// 验证码
-	code := c.PostForm("code")
-	num, _ := strconv.Atoi(code)
-	if num != 200 {
+	email := c.PostForm("email")
+	//language := c.PostForm("lan")
+	if len(email) == 0 {
 		response.Fail(c, util.ApiCode.ParamError, util.ApiMessage.ParamError)
+		return
+	}
+
+	smptServer := "smtp.163.com"
+	smptPort := 465
+	username := "xingtianwuganqi123@163.com"
+	password := "DQHEPDTSYPAVKEFB"
+	code := generateValidationCode(6)
+	// 对方的邮箱
+	recipient := email
+	subject := "【您的验证码】"
+	body := fmt.Sprintf("您的验证码为 %s,请在10分钟内使用。", code)
+
+	// 将code保存到redis，设置10分钟失效
+
+	err := sendEmail(recipient, subject, body, smptServer, smptPort, username, password)
+	if err != nil {
+		response.Fail(c, util.ApiCode.ServerError, util.ApiMessage.ServerError)
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{
 		"code": http.StatusOK,
-		"data": 2024,
+		"data": gin.H{},
 	})
 }
 
@@ -123,4 +142,34 @@ func UserPhoneLogin(c *gin.Context) {
 	} else {
 		response.Fail(c, 300, "密码错误")
 	}
+}
+
+// 获取验证码
+// 生成指定长度的随机数字验证码
+func generateValidationCode(length int) string {
+	var code string
+	for i := 0; i < length; i++ {
+		code += fmt.Sprintf("%d", rand.Intn(9))
+	}
+	return code
+}
+
+// 发送验证码
+func sendEmail(recipient string,
+	subject string,
+	body string,
+	smtpServier string,
+	smtpPort int,
+	username string,
+	password string) error {
+	m := gomail.NewMessage()
+	m.SetHeader("From", username)
+	m.SetHeader("To", recipient)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/html", body)
+
+	d := gomail.NewDialer(smtpServier, smtpPort, username, password)
+	d.SSL = true //使用ssl连接
+
+	return d.DialAndSend(m)
 }
