826a906034c7898bc2221924d38e98de8ce6bbce
jingjun <254032134@qq.com>
2024-06-21 11:13:12 +0800

    fix

--- pet-project/routers/user_router.go
+++ pet-project/routers/user_router.go
@@ -1,26 +1,17 @@
 package routers
 
 import (
-	"pet-project/handler/admin"
-	"pet-project/handler/api"
-
 	"github.com/gin-gonic/gin"
+	"pet-project/handler"
 )
 
 func RegisterUserRouter(r *gin.Engine) {
-
-	adminUserRouter := r.Group("/admin/user")
-	{
-		adminUserRouter.GET("/list", admin.UserList)
-
-	}
-
 	userRouter := r.Group("/v1/user")
 	{
-		userRouter.POST("/register", api.UserRegister)
-		userRouter.POST("/login", api.UserPhoneLogin)
-		userRouter.POST("/tencent/code", api.GetTencentCode)
-		userRouter.POST("/find/password", api.UserFindPassword)
+		userRouter.POST("/register", handler.UserRegister)
+		userRouter.POST("/login", handler.UserPhoneLogin)
+		userRouter.POST("/tencent/code", handler.GetTencentCode)
+		userRouter.POST("/find/password", handler.UserFindPassword)
 	}
 
 }
