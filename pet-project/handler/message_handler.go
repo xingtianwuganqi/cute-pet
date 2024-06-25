86e995377031b478f019ae2a3dd789cbb6d118f9
jingjun <254032134@qq.com>
2024-06-25 19:04:28 +0800

    Update message_handler.go

--- pet-project/handler/message_handler.go
+++ pet-project/handler/message_handler.go
@@ -16,13 +16,17 @@ func LikeMessageHandler(c *gin.Context) {
 		return
 	}
 	var findLike models.LikeMessageModel
-	findResult := db.DB.Model(models.LikeMessageModel{}).Where("like_id", likeMsg.LikeId).First(&findLike)
+	findResult := db.DB.Model(models.LikeMessageModel{}).Where("like_id = ?", likeMsg.LikeId).First(&findLike)
 	if errors.Is(findResult.Error, gorm.ErrRecordNotFound) {
 		createResult := db.DB.Create(&likeMsg)
 		if createResult.Error != nil {
 			response.Fail(c, response.ApiCode.CreateErr, response.ApiMsg.CreateErr)
 			return
 		}
+		// 创建一条消息
+		//MsgInfo := models.MessageModel{
+		//	MessageId: createResult.id
+		//}
 		response.Success(c, map[string]interface{}{})
 	} else {
 		insertResult := db.DB.Model(&findLike).Update("like_status", likeMsg.LikeStatus)
@@ -31,5 +35,29 @@ func LikeMessageHandler(c *gin.Context) {
 			return
 		}
 		response.Success(c, map[string]interface{}{})
+	}
+}
+
+func CollectionMessageHandler(c *gin.Context) {
+	var collection models.CollectionMessageModel
+	if err := c.ShouldBind(&collection); err != nil {
+		response.Fail(c, response.ApiCode.ParamErr, response.ApiMsg.ParamErr)
+		return
+	}
+	var findCollection models.CollectionMessageModel
+	findResult := db.DB.Model(models.CollectionMessageModel{}).Where("collection_id = ?", collection.CollectionId).First(&findCollection)
+	if errors.Is(findResult.Error, gorm.ErrRecordNotFound) {
+		createResult := db.DB.Create(&collection)
+		if createResult.Error != nil {
+			response.Fail(c, response.ApiCode.CreateErr, response.ApiMsg.CreateErr)
+			return
+		}
+		response.Success(c, map[string]interface{}{})
+	} else {
+		insertResult := db.DB.Model(&findCollection).Update("collection_status", collection.CollectionStatus)
+		if insertResult.Error != nil {
+			response.Fail(c, response.ApiCode.CreateErr, response.ApiMsg.CreateErr)
+			return
+		}
 	}
 }
